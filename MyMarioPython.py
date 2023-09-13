fafb3b3468b1e5dee5bd6e2e80182e4b40497bc7
Christian Encinares Bocz <144127538+xtianencinares@users.noreply.github.com>
2023-09-13 10:07:28 +0800

    Update MyMarioPython.py

--- MyMarioPython.py
+++ MyMarioPython.py
@@ -26,7 +26,7 @@ class Topscore:
         self.high_score = 0
     def top_score(self, score):
         if score > self.high_score:
-            self.high_score = score
+         self.high_score = score
     return self.high_score
 
 topscore = Topscore()
